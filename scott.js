ada4fe6c1f36fd45778a67b6f513e263cda17ebd
Scott <isrealscott679@gmail.com>
2019-07-15 14:01:32 +0200

    first commit

---
+++ scott.js
@@ -0,0 +1,10 @@
+<html>
+<head>
+<body>
+  <p class="id"Click here for a surprise.</p>
+  <button onclick="setTimeout(myFunction, 3000);"Surprise</button>
+   <script> function myFunction() {
+     alert('This is the surprise, that i used java script');
+   }</script>
+</body>
+</head></html>
